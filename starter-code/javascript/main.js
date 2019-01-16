b8e7e4450cd47488f18d561edfd8dc4f98cbf9f9
pablo <paugf89@gmail.com>
2019-01-16 09:20:20 +0100

    colors and text change but numbers are not print

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -51,10 +51,28 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+    if (btnLeft.innerHTML === "START") {
+        btnLeft.innerHTML = "STOP";
+    } else {
+        btnLeft.innerHTML = "START";
+        }
 
+    if (btnRight.innerHTML === "RESET") {
+        btnRight.innerHTML = "SPLIT";
+    } else {
+        btnRight.innerHTML = "RESET";
+        }
+
+    btnLeft.classList.toggle("start")
+    btnLeft.classList.toggle("stop")   
+    btnRight.classList.toggle("split")
+    btnRight.classList.toggle("reset")
+    // Set the `btnRight` button with the text SPLIT, and the class `btn split`
+       
+     
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    
 });
